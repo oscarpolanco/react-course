4f6b9728262e2a25a2d9fbe1614e707dd357a3d7
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-16 18:33:10 -0400

    Use props on the reusable component

--- adopt-me/scr/App.js
+++ adopt-me/scr/App.js
@@ -1,8 +1,8 @@
- const Pet = () => {
+ const Pet = (props) => {
      return React.createElement("div", {}, [
-         React.createElement("h1", {}, "Luna"),
-         React.createElement("h2", {}, "Dog"),
-         React.createElement("h2", {}, "Havanese"),
+         React.createElement("h1", {}, props.name),
+         React.createElement("h2", {}, props.animal),
+         React.createElement("h2", {}, props.breed),
      ])
  }
  
@@ -13,9 +13,21 @@
         { "id": "test" },
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet)
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Havanese"
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "Bird",
+                breed: "Cocktiel"
+            }),
+            React.createElement(Pet, {
+                name: "Doink",
+                animal: "Cat",
+                breed: "Mixed"
+            })
         ]
     );
 }
