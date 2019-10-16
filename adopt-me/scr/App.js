4058b9ecc7e8ac069ed7b2d6d254bd7f3167d99a
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-16 18:24:50 -0400

    Create an app file for my components and reuse a component

---
+++ adopt-me/scr/App.js
@@ -0,0 +1,26 @@
+ const Pet = () => {
+     return React.createElement("div", {}, [
+         React.createElement("h1", {}, "Luna"),
+         React.createElement("h2", {}, "Dog"),
+         React.createElement("h2", {}, "Havanese"),
+     ])
+ }
+ 
+ // your code is going to be here
+ const App = () => {
+    return React.createElement(
+        "div",
+        { "id": "test" },
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]
+    );
+}
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+);
