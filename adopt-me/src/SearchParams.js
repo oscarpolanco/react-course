ead16c88bc718724c3cb93172d8d81995763cbc2
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-23 18:48:38 -0400

    Add a async function to the form and list of browser that babel will handle

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -7,6 +7,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -21,7 +32,12 @@ const SearchParams = () => {
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPest();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
