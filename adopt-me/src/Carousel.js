6103ef3c39020506418bd50ebfc06470c25e4778
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-24 18:17:26 -0400

    Add a carousel component

---
+++ adopt-me/src/Carousel.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+
+  static getDrivedStateFromProps({ media }) {
+    let photos = ["http://placeholder.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
