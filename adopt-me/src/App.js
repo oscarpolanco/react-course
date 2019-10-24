f5a1ee16f98da032f31ab8173b9d699e24894c9c
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-24 14:31:29 -0400

    Add reach router

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import "regenerator-runtime/runtime";
+import Details from "./Details";
 
 // your code is going to be here
 const App = () => {
@@ -9,7 +11,10 @@ const App = () => {
     <React.StrictMode>
       <div>
         <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
