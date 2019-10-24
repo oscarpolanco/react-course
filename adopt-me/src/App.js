df484caf95f2315b4fb25dd47b97a1406e1bb6ad
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-24 14:40:26 -0400

    Print the react router info object on the details page and use link

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import "regenerator-runtime/runtime";
 import Details from "./Details";
@@ -10,7 +10,9 @@ const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Details = () => {
-  return <h1>hi lol</h1>;
+const Details = props => {
+  return (
+    <pre>
+      <code>{JSON.stringify(props, null, 4)}</code>
+    </pre>
+  );
 };
 
 export default Details;
