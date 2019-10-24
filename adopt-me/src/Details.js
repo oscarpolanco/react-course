3a2f1581285ccf0d07ecc71e06de67ba62c32f54
Oscar Polanco <opolanco@intellisys.com.do>
2019-10-24 16:46:49 -0400

    Update Details component to a class component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,11 +1,29 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = props => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {}
+}
 
 export default Details;
