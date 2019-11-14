86c5ea58cf1731983c6cc01f9551720cec13c39a
Oscar Polanco <opolanco@intellisys.com.do>
2019-11-14 18:28:55 -0400

    Template literals and hooks

--- adopt-me-v2/src/NavBar.js
+++ adopt-me-v2/src/NavBar.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">
-      🐩
-    </span>
-  </header>
-);
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+  return (
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: pink;
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+        `}
+        role="img"
+        aria-label="logo"
+      >
+        🐩
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
