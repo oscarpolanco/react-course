5de1d634ba89895aa9438d7088bfe48a5add0dc3
Oscar Polanco <opolanco@intellisys.com.do>
2019-11-14 18:39:47 -0400

    Animation

--- adopt-me-v2/src/NavBar.js
+++ adopt-me-v2/src/NavBar.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
 import colors from "./colors";
 
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 const NavBar = () => {
-  const [padding, setPadding] = useState(15);
+  const [padding] = useState(15);
   return (
     <header
-      onClick={() => setPadding(padding + 15)}
       css={css`
         background-color: ${colors.secondary};
         padding: ${padding}px;
@@ -17,7 +21,10 @@ const NavBar = () => {
       <span
         css={css`
           font-size: 60px;
+          display: block;
+          animation: 1s ${spin} linear infinite;
           &:hover {
+            animation: 1s ${spin} linear infinite reverse;
             text-decoration: underline;
           }
         `}
