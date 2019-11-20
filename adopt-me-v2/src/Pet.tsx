909eac79e55b90eec5b565982fe38b98ebdc1c55
Oscar Polanco <opolanco@intellisys.com.do>
2019-11-20 18:27:04 -0400

    Use props and state interfaces

--- adopt-me-v2/src/Pet.tsx
+++ adopt-me-v2/src/Pet.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
+import { Photo } from "@frontendmasters/pet";
 import { Link } from "@reach/router";
 
-export default function Pet({ name, animal, breed, media, location, id }) {
+interface IProps {
+  name: string;
+  animal: string;
+  breed: string;
+  media: Photo[];
+  location: string;
+  id: number;
+}
+
+const Pet: FunctionComponent<IProps> = ({
+  name,
+  animal,
+  breed,
+  media,
+  location,
+  id
+}) => {
   let hero = "http://placecorgi.com/300/300";
   if (media.length) {
     hero = media[0].small;
@@ -18,4 +35,6 @@ export default function Pet({ name, animal, breed, media, location, id }) {
       </div>
     </Link>
   );
-}
+};
+
+export default Pet;
