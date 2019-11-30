c5735182c3be8bf16c78a08f619589eaba2928e7
Oscar Polanco <opolanco@intellisys.com.do>
2019-11-30 15:29:34 -0400

    Eliminate redux from the app

--- adopt-me-v2/src/reducers/location.js
+++
@@ -1,7 +0,0 @@
-export default function location(state = "Seattle, WA", action) {
-  if (action.type === "CHANGE_LOCATION") {
-    return action.payload;
-  } else {
-    return state;
-  }
-}
--- adopt-me-v2/src/reducers/theme.js
+++
@@ -1,7 +0,0 @@
-export default function theme(state = "darkblue", action) {
-  if (action.type === "CHANGE_THEME") {
-    return action.payload;
-  } else {
-    return state;
-  }
-}
